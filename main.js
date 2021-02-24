fe522de24ff4c03388430d8514184e38a8d81fcd
petarPAN <petarburic554@gmail.com>
2021-02-24 22:25:17 +0100

    Change navbar after scrolling, with the condition

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', (event) => {
     let navbar = document.querySelector('nav');
     console.log(event)
-    navbar.classList.toggle('scrolling-active');
+    navbar.classList.toggle('scrolling-active', window.scrollY > 20);
 });
