4c48258b1d2c31c43b4b663931b081a4166ab8f2
petarPAN <petarburic554@gmail.com>
2021-02-25 20:36:58 +0100

    Add text typing effect

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
+// navbar animation
 window.addEventListener('scroll', (event) => {
     let navbar = document.querySelector('nav');
     console.log(event)
     navbar.classList.toggle('scrolling-active', window.scrollY > 20);
 });
+
+// typewriting
+const contnet = ['Student', 'Web Developer'];
+let index = 0;
+let count = 0;
+let currentContnet = '';
+let letter = '';
+
+(function typing() {
+    if (count === contnet.length) {
+        count = 0;
+    }
+
+    currentContnet = contnet[count];
+    letter = currentContnet.slice(0, ++index);
+
+    document.querySelector('.typing').textContent = letter;
+    if (currentContnet.length === letter.length) {
+        count++
+        index = 0;
+    }
+    setTimeout(typing, 200);
+}());
