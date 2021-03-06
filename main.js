2ac89cb4dcb94e4aa1aef95027911ef65db149f6
petarPAN <petarburic554@gmail.com>
2021-03-06 12:53:54 +0100

    change the color of nav background and nav elements, after scrolling

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 // navbar animation
 window.addEventListener('scroll', (event) => {
     let navbar = document.querySelector('nav');
-    console.log(event)
-    navbar.classList.toggle('scrolling-active', window.scrollY > 20);
+    navbar.classList.toggle('nav-scrolling-bg', window.scrollY > 20);
+
+    let logo = document.querySelector('a.navbar-brand');
+    console.log(logo)
+    let logoChild = logo.querySelectorAll('span');
+    console.log(logoChild)
+    //console.log(logoChild)
+    for (let a = 0; a < logoChild.length; a++) {
+        let spanEl = logoChild[a];
+        console.log(spanEl)
+        spanEl.classList.toggle('nav-scrolling-color', window.scrollY > 20)
+    }
+
+    let ulElement = document.getElementsByClassName('navbar-nav');
+    //console.log(ulElement)
+    for (let i = 0; i < ulElement.length; i++) {
+        let parentEl = ulElement[i];
+        let child = parentEl.getElementsByClassName('nav-item');
+        //console.log(child)
+        for (let j = 0; j < child.length; j++) {
+            let liEl = child[j]
+            //console.log(liEl)
+            let child2 = liEl.getElementsByClassName('nav-link');
+            //console.log(child2)
+            for (let k = 0; k < child2.length; k++) {
+                let aEl = child2[k];
+                //console.log(aEl)
+                aEl.classList.toggle('nav-scrolling-color', window.scrollY > 20);
+            }
+        }
+    }
 });
 
 // typewriting
